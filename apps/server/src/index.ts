f11bf0793fe738d79be56b8dce3f61add1a1233f
vhlima <vhlima2000@gmail.com>
2023-07-25 14:34:43 -0300

    feat: Add Postgres connection

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,4 +1,10 @@
+import 'reflect-metadata';
+
+import dotenv from "dotenv"
+dotenv.config()
+
 import express from 'express';
+import { connectPostgres } from '@/infra/data-sources';
 
 const app = express();
 const port = 3000
@@ -8,5 +14,7 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
+  connectPostgres();
+
   console.log(`Example app listening on port ${port}`);
 })
