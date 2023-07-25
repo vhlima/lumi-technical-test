527ed97ddacd845955770b5aaa53d6ffd8eaed85
vhlima <vhlima2000@gmail.com>
2023-07-25 19:27:45 -0300

    feat: Add LatestInvoicesList component

---
+++ apps/web/src/pages/Home/components/LatestInvoicesList/index.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { Invoice } from "../../../../interfaces";
+import { ListLatestInvoicesService } from "../../../../services";
+import { Box, CircularProgress, List, Typography } from "@mui/material";
+import InvoiceItem from "./components/InvoiceItem";
+
+const LatestInvoicesList: React.FC = () => {
+  const [invoices, setInvoices] = useState<Invoice[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      const listLatestInvoicesService = new ListLatestInvoicesService();
+      const latestInvoicesResponse = await listLatestInvoicesService.execute();
+      setInvoices(latestInvoicesResponse);
+      setLoading(false);
+    })();
+  }, []);
+
+  if (loading || invoices.length === 0) {
+    return (
+      <Box sx={{ display: "flex", justifyContent: "center", marginTop: 2 }}>
+        {loading && <CircularProgress />}
+        {!loading && invoices.length === 0 && (
+          <Typography>No invoice was found.</Typography>
+        )}
+      </Box>
+    );
+  }
+
+  return (
+    <List disablePadding>
+      {invoices.map((invoice) => (
+        <InvoiceItem key={`invoice-${invoice.id}`} {...invoice} />
+      ))}
+    </List>
+  );
+};
+
+export default LatestInvoicesList;
--- apps/web/src/pages/Home/index.tsx
+++ apps/web/src/pages/Home/index.tsx
@@ -1,10 +1,21 @@
-import InfoCardList from "./components/InfoCardList";
+import { Box, Divider, Typography } from "@mui/material";
 import Layout from "../../layout";
+
+import InfoCardList from "./components/InfoCardList";
+import LatestInvoicesList from "./components/LatestInvoicesList";
 
 const HomePage: React.FC = () => {
   return (
     <Layout>
       <InfoCardList />
+
+      <Box sx={{ marginTop: 4 }}>
+        <Typography variant="h5" gutterBottom>
+          Latest Invoices
+        </Typography>
+        <Divider />
+        <LatestInvoicesList />
+      </Box>
     </Layout>
   );
 };
