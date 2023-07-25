61e364a3be8ab719417b9a021fe669830f126a8d
vhlima <vhlima2000@gmail.com>
2023-07-25 14:10:43 -0300

    chore: Transform text content in rows before returning

--- apps/server/src/adapters/load-pdf-adapter.ts
+++ apps/server/src/adapters/load-pdf-adapter.ts
@@ -5,9 +5,40 @@ import { PDFDocument } from "@/domain/entities";
 import { TextItem } from "pdfjs-dist/types/src/display/api";
 
 export class LoadPDFAdapter implements LoadPDF {
+  /*
+    This function converts pdf files text items into rows for better information
+    extraction. The only purpose of this function is to make it easier and precise 
+    to read informations.
+  */
+  private transformTextContentItemsToRows(items: TextItem[]): string[][] {
+    const rows: string[][] = [[]];
+
+    let lastItem = null;
+    let currentRowIndex = 0;
+
+    for (let i = 0; i < items.length; i++) {
+      let item = items[i];
+
+      if (lastItem !== null && lastItem.str[lastItem.str.length - 1] !== " ") {
+        if (item.transform[5] < lastItem.transform[5]) {
+          currentRowIndex++;
+        }
+      }
+
+      if (!rows[currentRowIndex]) {
+        rows[currentRowIndex] = [];
+      }
+
+      rows[currentRowIndex].push(item.str);
+      lastItem = item;
+    }
+
+    return rows;
+  }
+
   public async execute(path: string): Promise<PDFDocument> {
     const pdfData = new Uint8Array(fs.readFileSync(path));
-    
+
     const loadingTask = getDocument({ data: pdfData, useSystemFonts: true });
 
     const document = await loadingTask.promise;
@@ -21,10 +52,12 @@ export class LoadPDFAdapter implements LoadPDF {
         getTextContent: async () => {
           const content = await page.getTextContent();
 
-          return content.items as TextItem[];
-        }
-      }
-    }
+          return this.transformTextContentItemsToRows(
+            content.items as TextItem[]
+          );
+        },
+      };
+    };
 
     return pdfDocument;
   }
