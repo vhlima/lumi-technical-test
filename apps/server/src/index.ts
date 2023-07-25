b20a4d88845832b4a53f3a4a17164f72cef5360e
vhlima <vhlima2000@gmail.com>
2023-07-25 19:01:45 -0300

    feat: Implement cors policy on server

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -4,6 +4,7 @@ import dotenv from "dotenv"
 dotenv.config()
 
 import express from 'express';
+import cors from 'cors';
 import { connectPostgres } from '@/infra/data-sources';
 import serverRouter from './routes';
 
@@ -11,6 +12,12 @@ const app = express();
 const port = 4000
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.WEB_APP_URL,
+    credentials: true,
+  }),
+);
 app.use(serverRouter);
 
 app.listen(port, () => {
