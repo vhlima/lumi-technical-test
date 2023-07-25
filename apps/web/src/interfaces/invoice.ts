ad89b7b11084be92808176a14ad252a0fc544dc6
vhlima <vhlima2000@gmail.com>
2023-07-25 19:27:07 -0300

    chore: Add Invoice types

---
+++ apps/web/src/interfaces/invoice.ts
@@ -0,0 +1,26 @@
+export interface InvoiceExpense {
+  id: number;
+  name: string;
+  price: number;
+  quantity?: number;
+  unitaryPrice?: number; 
+  unitaryTaxPrice?: number;
+  measurementUnit?: string;
+  invoice: Invoice;
+}
+
+export interface Invoice {
+  id: number;
+
+  clientId: string;
+
+  installationNumber: number;
+
+  price: number;
+
+  expenses: InvoiceExpense[];
+
+  relativeTo: string;
+
+  expiresAt: string;
+}
