38cf0633e7af391b7aaf55698189a7c974b6ae22
vhlima <vhlima2000@gmail.com>
2023-07-26 17:18:06 -0300

    test: Ensure Invoice Expenses are being parsed correctly

---
+++ apps/server/tests/data/services/parse-invoice-expense.spec.ts
@@ -0,0 +1,31 @@
+import { ParseExpensesService } from "@/data/services";
+import {
+  mockInvoice,
+  mockInvoiceExpense,
+  mockPdfTextContent,
+} from "@/tests/domain/mocks";
+import { InvoiceExpenseValidator } from "@/validation/validators";
+import { faker } from "@faker-js/faker";
+
+const createSut = () => {
+  const sut = new ParseExpensesService(new InvoiceExpenseValidator());
+  return sut;
+};
+
+describe("ParseInvoiceExpensesService", () => {
+  test("Should parse Invoice Expenses correctly", async () => {
+    const sut = createSut();
+
+    const mockedExpenses = Array.from({
+      length: faker.number.int({ min: 1, max: 5 }),
+    }).map(() => mockInvoiceExpense());
+
+    const pdfTextContent = mockPdfTextContent(mockInvoice(mockedExpenses));
+
+    const createdExpenses = sut.execute(pdfTextContent);
+
+    expect(createdExpenses).toEqual(
+      mockedExpenses.map(({ id, ...expense }) => expense)
+    );
+  });
+});
