5a8f51e7a65549dfa5eb1e0f9c141ea7ebb443b8
vhlima <vhlima2000@gmail.com>
2023-07-26 15:58:26 -0300

    chore: Invoice mock was not working with correct date

--- apps/server/tests/domain/mocks/mock-invoice.ts
+++ apps/server/tests/domain/mocks/mock-invoice.ts
@@ -2,14 +2,27 @@ import { Invoice } from "@/domain/entities";
 import { faker } from "@faker-js/faker";
 
 export const mockInvoice = (): Invoice => {
+  const expiresAt = faker.date.future();
+  expiresAt.setHours(0);
+  expiresAt.setMinutes(0);
+  expiresAt.setSeconds(0);
+  expiresAt.setMilliseconds(0);
+
+  const relativeTo = faker.date.recent();
+  relativeTo.setDate(1);
+  relativeTo.setHours(0);
+  relativeTo.setMinutes(0);
+  relativeTo.setSeconds(0);
+  relativeTo.setMilliseconds(0);
+
   const invoice: Invoice = {
     clientId: faker.number.int(),
     expenses: [],
     price: 0,
-    expiresAt: faker.date.future(),
+    expiresAt,
     installationNumber: faker.number.int(),
     id: faker.number.int(),
-    relativeTo: faker.date.recent(),
+    relativeTo,
   };
 
   return invoice;
