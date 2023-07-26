ee7aa9d9259b38c4d9400a362f238b32d077a973
vhlima <vhlima2000@gmail.com>
2023-07-26 20:26:05 -0300

    style: Add light theme

---
+++ apps/web/src/theme/light.ts
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material";
+
+export default createTheme({
+  palette: {
+    primary: {
+      main: '#023b21',
+    },
+    text: {
+      primary: "#212b36",
+      secondary: "#637381",
+    },
+  },
+});
