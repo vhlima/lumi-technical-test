2d60bb8d059800dcc6d2b7de8400c16d1abb856e
vhlima <vhlima2000@gmail.com>
2023-07-26 16:28:39 -0300

    chore: Add optional expenses field to invoice mock

--- apps/server/tests/domain/mocks/mock-invoice.ts
+++ apps/server/tests/domain/mocks/mock-invoice.ts
@@ -1,7 +1,8 @@
-import { Invoice } from "@/domain/entities";
+import { Invoice, InvoiceExpense } from "@/domain/entities";
 import { faker } from "@faker-js/faker";
+import { mockInvoiceExpense } from "./mock-invoice-expense";
 
-export const mockInvoice = (): Invoice => {
+export const mockInvoice = (expenses?: InvoiceExpense[]): Invoice => {
   const expiresAt = faker.date.future();
   expiresAt.setHours(0);
   expiresAt.setMinutes(0);
@@ -24,6 +25,17 @@ export const mockInvoice = (): Invoice => {
     id: faker.number.int(),
     relativeTo,
   };
+
+  invoice.expenses = expenses
+    ? expenses
+    : Array.from({
+        length: faker.number.int({ min: 1, max: 5 }),
+      }).map(() => mockInvoiceExpense(invoice));
+
+  invoice.price = invoice.expenses.reduce(
+    (acc, expense) => (acc += expense.price),
+    0
+  );
 
   return invoice;
 };
