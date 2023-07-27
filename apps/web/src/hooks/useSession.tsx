e53e69e275e8a557f4e001d74db3acfe55ba1beb
vhlima <vhlima2000@gmail.com>
2023-07-27 16:15:56 -0300

    feat: Add session persistance

--- apps/web/src/hooks/useSession.tsx
+++ apps/web/src/hooks/useSession.tsx
@@ -3,10 +3,12 @@ import {
   createContext,
   useCallback,
   useContext,
+  useEffect,
   useMemo,
   useState,
 } from "react";
 import { Client } from "../interfaces";
+import { FindClientService } from "../services";
 
 interface SessionContextData {
   client?: Client;
@@ -15,6 +17,11 @@ interface SessionContextData {
 
 const SessionContext = createContext({} as SessionContextData);
 
+/*
+  Usually, we won't be persisting client id directly from localStorage,
+  but since this is not a key feature of this MVP, we are going this way
+  just to same some time.
+*/
 export const SessionProvider: React.FC<PropsWithChildren> = (props) => {
   const { children } = props;
 
@@ -23,9 +30,36 @@ export const SessionProvider: React.FC<PropsWithChildren> = (props) => {
   const signIn = useCallback(
     (client: Client) => {
       setClient(client);
+      localStorage.setItem("client-id", String(client.id));
     },
     [setClient]
   );
+
+  useEffect(() => {
+    (async () => {
+      if (client) {
+        return; 
+      }
+
+      const localStorageClientId = localStorage.getItem("client-id");
+
+      if (localStorageClientId) {
+        try {
+          const findClientService = new FindClientService();
+          const client = await findClientService.execute(
+            parseInt(localStorageClientId, 10)
+          );
+
+          if (!client) {
+            localStorage.removeItem("cleint-id");
+            setClient(undefined);
+            return;
+          }
+          setClient(client);
+        } catch (err) {}
+      }
+    })();
+  }, [client]);
 
   const contextValue = useMemo(() => ({ client, signIn }), [client, signIn]);
 
--- apps/web/src/services/index.ts
+++ apps/web/src/services/index.ts
@@ -2,3 +2,4 @@ export * from './list-latest-invoices';
 export * from './list-invoices';
 export * from './create-invoice';
 export * from './find-client-profile';
+export * from './find-client';
