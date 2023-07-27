06b74e523a9e53a51b5f6131f176b3485945e581
vhlima <vhlima2000@gmail.com>
2023-07-27 16:37:38 -0300

    chore: Refactor ClientSelectorDialog component

--- apps/web/src/pages/Home/components/ClientSelector/components/ClientSelectorDialog/index.tsx
+++ apps/web/src/pages/Home/components/ClientSelector/components/ClientSelectorDialog/index.tsx
@@ -1,17 +1,9 @@
-import {
-  Avatar,
-  Dialog,
-  DialogTitle,
-  List,
-  ListItem,
-  ListItemAvatar,
-  ListItemButton,
-  ListItemText,
-} from "@mui/material";
-import { useState } from "react";
+import { Dialog, DialogTitle, List } from "@mui/material";
+import { useEffect, useState } from "react";
 import { Client } from "../../../../../../interfaces";
-import { Person } from "@mui/icons-material";
 import { useSession } from "../../../../../../hooks/useSession";
+import { ListClientsService } from "../../../../../../services";
+import ClientItem from "./components/ClientItem";
 
 interface Props {
   open: boolean;
@@ -23,58 +15,31 @@ const ClientSelectorDialog: React.FC<Props> = (props) => {
 
   const { client: session, signIn } = useSession();
 
-  const [clientList, setClientList] = useState<Client[]>(
-    Array.from({ length: 5 }).map((_, index) => ({
-      id: 7202788969 + index,
-      fullName: "Bronyer Tozatti Ferreira",
-      addresses: [
-        {
-          id: 10,
-          streetAddress: "Rua Joao De Assis Martins 71 In",
-          district: "Centro Sul",
-          zipCode: "35182-036",
-          state: "MG",
-          city: "Timoteo",
-        },
-      ],
-    }))
-  );
+  const [clientList, setClientList] = useState<Client[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const listClientsService = new ListClientsService();
+      const clientListResponse = await listClientsService.execute();
+      setClientList(clientListResponse);
+    })();
+  }, []);
 
   return (
     <Dialog onClose={onClose} open={open}>
       <DialogTitle>Switch accounts</DialogTitle>
       <List sx={{ pt: 0 }}>
-        {clientList.map((client) => {
-          const isSameSession = session && session.id === client.id;
-          return (
-            <ListItem
-              sx={{
-                ...(isSameSession ? { backgroundColor: "primary.main" } : {}),
-              }}
-              key={`client-${client.id}`}
-              disableGutters
-            >
-              <ListItemButton
-                onClick={() => {
-                  signIn(client);
-                  onClose();
-                }}
-              >
-                <ListItemAvatar>
-                  <Avatar sx={{ bgcolor: "blue.100", color: "blue.600" }}>
-                    <Person />
-                  </Avatar>
-                </ListItemAvatar>
-                <ListItemText
-                  sx={{
-                    ...(isSameSession ? { color: "white" } : {}),
-                  }}
-                  primary={client.fullName}
-                />
-              </ListItemButton>
-            </ListItem>
-          );
-        })}
+        {clientList.map((client) => (
+          <ClientItem
+            key={`client-${client.id}`}
+            fullName={client.fullName}
+            active={session && session.id === client.id}
+            onClick={() => {
+              signIn(client);
+              onClose();
+            }}
+          />
+        ))}
       </List>
     </Dialog>
   );
