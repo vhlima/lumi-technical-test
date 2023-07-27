a3568860dfc68ab9843929013d8b9e991a18f0fd
vhlima <vhlima2000@gmail.com>
2023-07-27 02:24:06 -0300

    chore: Replace scoped function with util

--- apps/server/src/data/services/create-client.ts
+++ apps/server/src/data/services/create-client.ts
@@ -1,13 +1,10 @@
 import { Client } from "@/domain/entities";
 import { CreateClient, CreateClientData } from "@/domain/usecases";
 import { IClientsRepository } from "@/data/contracts";
+import { firstLetterUppercase } from "@/utils/string-utils";
 
 export class CreateClientService implements CreateClient {
   constructor(private readonly clientsRepository: IClientsRepository) {}
-
-  private firstLetterUppercase(str: string): string {
-    return `${str[0].toUpperCase()}${str.slice(1, str.length).toLowerCase()}`;
-  }
 
   public async execute(data: CreateClientData): Promise<Client> {
     const clientExists = await this.clientsRepository.findById(data.id);
@@ -20,7 +17,7 @@ export class CreateClientService implements CreateClient {
       ...data,
       fullName: data.fullName
         .split(" ")
-        .map((name) => this.firstLetterUppercase(name))
+        .map((name) => firstLetterUppercase(name))
         .join(" "),
     });
     return client;
