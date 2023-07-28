b973b78f5e656fb72776f9908c459d0479adad4d
vhlima <vhlima2000@gmail.com>
2023-07-28 17:14:39 -0300

    feat: Add useAddress hook

--- apps/web/src/layout/index.tsx
+++ apps/web/src/layout/index.tsx
@@ -1,15 +1,27 @@
 import { PropsWithChildren } from "react";
 import { Box, Container } from "@mui/material";
 import Navbar from "./components/Navbar";
+import { useSession } from "../hooks/useSession";
+import { AddressProvider } from "../hooks/useAddress";
 
 const Layout: React.FC<PropsWithChildren> = (props) => {
+  const { client: session } = useSession();
+
   const { children } = props;
 
-  return (
+  const layout = (
     <Box>
       <Navbar />
       <Container sx={{ marginTop: 4 }}>{children}</Container>
     </Box>
+  );
+
+  if (!session) {
+    return layout;
+  }
+
+  return (
+    <AddressProvider address={session.addresses[0]}>{layout}</AddressProvider>
   );
 };
 
