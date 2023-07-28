39fc0d7c1602c7a76dafa725777f9305b327aa94
vhlima <vhlima2000@gmail.com>
2023-07-28 18:02:06 -0300

    feat: Add message for empty latest invoices

--- apps/web/src/pages/Home/components/LatestInvoicesSection/components/LatestInvoicesList/index.tsx
+++ apps/web/src/pages/Home/components/LatestInvoicesSection/components/LatestInvoicesList/index.tsx
@@ -1,14 +1,24 @@
-import { List } from "@mui/material";
+import { List, Typography } from "@mui/material";
 import InvoiceItem from "../../../../../../components/InvoiceItem";
 import { useInvoiceList } from "../../../../hooks/useInvoiceList";
 
 const LatestInvoicesList: React.FC = () => {
   const { invoices } = useInvoiceList();
 
-  const latestInvoices = invoices.sort(
-    (i1, i2) =>
-      new Date(i2.relativeTo).getTime() - new Date(i1.relativeTo).getTime()
-  ).slice(0, 3);
+  const latestInvoices = invoices
+    .sort(
+      (i1, i2) =>
+        new Date(i2.relativeTo).getTime() - new Date(i1.relativeTo).getTime()
+    )
+    .slice(0, 3);
+
+  if (latestInvoices.length === 0) {
+    return (
+      <Typography sx={{ marginTop: 2, textAlign: "center" }}>
+        No invoice was found.
+      </Typography>
+    );
+  }
 
   return (
     <List disablePadding>
