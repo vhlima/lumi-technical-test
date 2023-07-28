4c0926b0d0da5090bdd2229737be31748c27abba
vhlima <vhlima2000@gmail.com>
2023-07-27 22:34:17 -0300

    feat: Implement address select on consumption section

--- apps/web/src/pages/Home/components/ConsumptionSection/components/ConsumptionChart/index.tsx
+++ apps/web/src/pages/Home/components/ConsumptionSection/components/ConsumptionChart/index.tsx
@@ -24,11 +24,12 @@ ChartJS.register(
 
 interface Props {
   label: string;
+  filter?: (invoices: Invoice[]) => Invoice[];
   onValueIncrement: (invoice: Invoice, currentAmount: number) => number;
 }
 
 const ConsumptionChart: React.FC<Props> = (props) => {
-  const { label, onValueIncrement } = props;
+  const { label, filter, onValueIncrement } = props;
 
   const { invoices } = useInvoiceList();
 
@@ -44,8 +45,10 @@ const ConsumptionChart: React.FC<Props> = (props) => {
   const convertInvoicesToValue = (invoices: Invoice[]) => {
     const invoicesByMonth: number[] = [];
 
-    for (let i = 0; i < invoices.length; i++) {
-      const invoice = invoices[i];
+    const invoicesFiltered = filter ? filter(invoices) : invoices;
+
+    for (let i = 0; i < invoicesFiltered.length; i++) {
+      const invoice = invoicesFiltered[i];
 
       const date = new Date(invoice.relativeTo);
       if (date.getFullYear() !== new Date(Date.now()).getFullYear()) {
