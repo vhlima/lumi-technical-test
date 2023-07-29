003fdb46f5bbd8d70aefe35efd4b4d8c82f19186
vhlima <vhlima2000@gmail.com>
2023-07-29 18:35:53 -0300

    test: Ensure empty list message is being rendered correctly

--- apps/web/src/components/AddressSelect/components/AddressSelector/index.spec.tsx
+++ apps/web/src/components/AddressSelect/components/AddressSelector/index.spec.tsx
@@ -11,14 +11,14 @@ type SutType = {
 };
 
 /* eslint-disable */
-const createSut = (): SutType => {
+const createSut = (empty?: boolean): SutType => {
   const addresses = Array.from({
     length: faker.number.int({ min: 1, max: 5 }),
   }).map(() => mockClientAddress());
 
   const sut = render(
     <AddressProvider address={addresses[0]}>
-      <AddressSelect open addresses={addresses} onClose={() => ({})} />
+      <AddressSelect open addresses={empty ? [] : addresses} onClose={() => ({})} />
     </AddressProvider>
   );
 
@@ -35,5 +35,17 @@ describe("AddressSelector", () => {
     const listElement = sut.getByTestId("address-selector-list");
     expect(listElement).toBeInTheDocument();
     expect(listElement.childElementCount).toEqual(addresses.length);
+
+    const emptyElement = sut.queryByTestId("address-selector-empty");
+    expect(emptyElement).not.toBeInTheDocument();
+  });
+  test("Should render empty list message if address list is empty", () => {
+    const { sut } = createSut(true);
+
+    const listElement = sut.queryByTestId("address-selector-list");
+    expect(listElement).not.toBeInTheDocument();
+
+    const emptyElement = sut.getByTestId("address-selector-empty");
+    expect(emptyElement).toBeInTheDocument();
   });
 });
