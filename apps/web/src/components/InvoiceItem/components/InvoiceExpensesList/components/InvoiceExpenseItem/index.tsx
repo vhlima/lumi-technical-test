6bdc683b136f9c152bec3b7e7d02a840f5d9d054
vhlima <vhlima2000@gmail.com>
2023-07-29 00:38:14 -0300

    test: Ensure invoice expense details are being rendered correctly

--- apps/web/src/components/InvoiceItem/components/InvoiceExpensesList/components/InvoiceExpenseItem/index.tsx
+++ apps/web/src/components/InvoiceItem/components/InvoiceExpensesList/components/InvoiceExpenseItem/index.tsx
@@ -1,5 +1,6 @@
 import { ListItem, Stack, Typography } from "@mui/material";
 import { InvoiceExpense } from "../../../../../../interfaces";
+import { parseToBRL } from "../../../../../../utils/currency-parser";
 
 interface Props {
   expense: InvoiceExpense;
@@ -17,30 +18,46 @@ const InvoiceExpenseItem: React.FC<Props> = (props) => {
       }}
       divider
     >
-      <Typography sx={{ color: "text.primary" }}>{expense.name}</Typography>
+      <Typography
+        sx={{ color: "text.primary" }}
+        data-testid="invoice-expense-name"
+      >
+        {expense.name}
+      </Typography>
 
       <Stack direction="column">
         <Typography variant="body2" sx={{ color: "text.secondary" }}>
-          Price: R$ {expense.price}
+          {`Price: `}
+          <span data-testid="invoice-expense-price">
+            {parseToBRL(expense.price)}
+          </span>
         </Typography>
 
         {expense.quantity && (
           <Typography variant="body2" sx={{ color: "text.secondary" }}>
-            {`Consumption: ${expense.quantity}${
-              expense.measurementUnit ? ` ${expense.measurementUnit}` : ""
-            }`}
+            {`Consumption: `}
+            <span data-testid="invoice-expense-quantity">
+              {expense.quantity}
+            </span>
+            {expense.measurementUnit ? ` ${expense.measurementUnit}` : ""}
           </Typography>
         )}
 
         {expense.unitaryPrice && (
           <Typography variant="body2" sx={{ color: "text.secondary" }}>
-            Unitary Price: {expense.unitaryPrice}
+            {`Unitary Price: `}
+            <span data-testid="invoice-expense-unitary-price">
+              {expense.unitaryPrice}
+            </span>
           </Typography>
         )}
 
         {expense.unitaryTaxPrice && (
           <Typography variant="body2" sx={{ color: "text.secondary" }}>
-            Unitary Tax Price: {expense.unitaryTaxPrice}
+            {`Unitary Tax Price: `}
+            <span data-testid="invoice-expense-unitary-tax-price">
+              {expense.unitaryTaxPrice}
+            </span>
           </Typography>
         )}
       </Stack>
