8e10482734d6842b68a07c2ed4852464d06173a0
vhlima <vhlima2000@gmail.com>
2023-07-29 00:50:17 -0300

    test: Ensure empty text is being rendered when selector list is empty

--- apps/web/src/components/Selector/index.spec.tsx
+++ apps/web/src/components/Selector/index.spec.tsx
@@ -4,18 +4,30 @@ import { faker } from "@faker-js/faker";
 
 type SutType = {
   title: string;
+  emptyText: string;
   sut: RenderResult;
 };
 
 /* eslint-disable */
-const createSut = (): SutType => {
+const createSut = (childCount = 0): SutType => {
   const title = faker.lorem.lines(1);
+  const emptyText = faker.lorem.lines(1);
 
-  const sut = render(<Selector open title={title} onClose={() => ({})} />);
+  const sut = render(
+    <Selector
+      open
+      title={title}
+      emptyText={childCount === 0 ? emptyText : undefined}
+      onClose={() => ({})}
+    >
+      {childCount > 0 && <span>{childCount}</span>}
+    </Selector>
+  );
 
   return {
     sut,
     title,
+    emptyText,
   };
 };
 
@@ -26,5 +38,15 @@ describe("Selector", () => {
     const titleElement = sut.getByTestId("selector-title");
     expect(titleElement).toBeInTheDocument();
     expect(titleElement.textContent).toEqual(title);
+  });
+  test("Should render empty text if list is empty", () => {
+    const { sut, emptyText } = createSut();
+
+    const emptyElement = sut.getByTestId("selector-empty");
+    expect(emptyElement).toBeInTheDocument();
+    expect(emptyElement.textContent).toEqual(emptyText);
+
+    const listElement = sut.queryByTestId("selector-list");
+    expect(listElement).not.toBeInTheDocument();
   });
 });
