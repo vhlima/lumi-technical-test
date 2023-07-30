692494109a86f0fe317b9637a3e2aca3eed2c441
vhlima <vhlima2000@gmail.com>
2023-07-30 01:43:49 -0300

    fix: Test case not working

--- apps/web/cypress/e2e/unauth.cy.ts
+++ apps/web/cypress/e2e/unauth.cy.ts
@@ -2,18 +2,18 @@ describe("Home page unauthenticated", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000");
   });
-  test("Should display the correct header text", () => {
+  it("Should display the correct header text", () => {
     cy.contains(
       "Welcome! Begin your journey by uploading your first invoice."
     ).should("be.visible");
   });
-  test("Should have two links in the navigation bar", () => {
+  it("Should have two links in the navigation bar", () => {
     cy.get("a.MuiButton-root.MuiButton-text.MuiButton-textPrimary").should(
       "have.length",
       2
     );
   });
-  test("Should show a file upload button", () => {
+  it("Should show a file upload button", () => {
     cy.get(
       "button.MuiButton-root.MuiButton-contained.MuiButton-containedPrimary"
     )
