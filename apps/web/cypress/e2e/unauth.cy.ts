6bdb1ba31cb22553597eeafd7d0c1e55e7ce7144
vhlima <vhlima2000@gmail.com>
2023-07-30 01:43:10 -0300

    chore: Fix some test typos

--- apps/web/cypress/e2e/unauth.cy.ts
+++ apps/web/cypress/e2e/unauth.cy.ts
@@ -1,18 +1,23 @@
-describe('Home page unauthenticated', () => {
+describe("Home page unauthenticated", () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000');
+    cy.visit("http://localhost:3000");
   });
-  it('should display the correct header text', () => {
-    cy.contains('Welcome! Begin your journey by uploading your first invoice.')
-      .should('be.visible');
+  test("Should display the correct header text", () => {
+    cy.contains(
+      "Welcome! Begin your journey by uploading your first invoice."
+    ).should("be.visible");
   });
-  it('should have two links in the navigation bar', () => {
-    cy.get('a.MuiButton-root.MuiButton-text.MuiButton-textPrimary')
-      .should('have.length', 2);
+  test("Should have two links in the navigation bar", () => {
+    cy.get("a.MuiButton-root.MuiButton-text.MuiButton-textPrimary").should(
+      "have.length",
+      2
+    );
   });
-  it('should show a file upload button', () => {
-    cy.get('button.MuiButton-root.MuiButton-contained.MuiButton-containedPrimary')
-      .should('be.visible')
-      .contains('Upload invoice');
+  test("Should show a file upload button", () => {
+    cy.get(
+      "button.MuiButton-root.MuiButton-contained.MuiButton-containedPrimary"
+    )
+      .should("be.visible")
+      .contains("Upload invoice");
   });
 });
