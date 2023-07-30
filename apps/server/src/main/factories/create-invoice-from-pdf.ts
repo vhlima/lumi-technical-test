910d2f70f738ef16f2a74db9db7e50edaeca0b16
vhlima <vhlima2000@gmail.com>
2023-07-29 23:43:15 -0300

    chore: Replace invoice parsers manual imports to start using factory

--- apps/server/src/main/factories/create-invoice-from-pdf.ts
+++ apps/server/src/main/factories/create-invoice-from-pdf.ts
@@ -1,12 +1,7 @@
 import { LoadPDFAdapter } from "@/adapters";
 import {
   CreateInvoiceFromPDFService,
-  InvoiceParsersService,
   LabelMapperService,
-  ParseClientAddressService,
-  ParseClientService,
-  ParseExpensesService,
-  ParseInvoiceService,
 } from "@/data/services";
 import {
   ClientsAddressesRepository,
@@ -14,24 +9,14 @@ import {
   InvoicesExpensesRepository,
   InvoicesRepository,
 } from "@/infra/repositories";
-import {
-  ClientAddressValidator,
-  ClientValidator,
-  InvoiceExpenseValidator,
-  InvoiceValidator,
-} from "@/validation/validators";
+import { getInvoiceParsersService } from "./invoice-parsers";
 
 export function getCreateInvoiceFromPDFService(): CreateInvoiceFromPDFService {
   const labelMapper = new LabelMapperService();
 
   const service = new CreateInvoiceFromPDFService(
     new LoadPDFAdapter(),
-    new InvoiceParsersService(
-      new ParseClientService(new ClientValidator(), labelMapper),
-      new ParseInvoiceService(new InvoiceValidator(), labelMapper),
-      new ParseExpensesService(new InvoiceExpenseValidator(), labelMapper),
-      new ParseClientAddressService(new ClientAddressValidator(), labelMapper)
-    ),
+    getInvoiceParsersService(),
     new ClientsRepository(),
     new InvoicesRepository(),
     new InvoicesExpensesRepository(),
