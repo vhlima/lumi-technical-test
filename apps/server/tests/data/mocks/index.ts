c8fc359c521a1cf4e0dca3dcc60315c38a476a89
vhlima <vhlima2000@gmail.com>
2023-07-29 23:44:39 -0300

    chore: Remove dead code

--- apps/server/tests/data/mocks/index.ts
+++ apps/server/tests/data/mocks/index.ts
@@ -1,5 +1,4 @@
 export * from './mock-invoices-repository';
-export * from './mock-load-pdf';
 export * from './mock-clients-repository';
 export * from './mock-invoices-expenses-repository';
 export * from './mock-clients-addresses-repository';
