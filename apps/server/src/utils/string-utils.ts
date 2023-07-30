cbb986e1d2df8d15af337e1d798df2e05c7743e1
vhlima <vhlima2000@gmail.com>
2023-07-29 22:02:21 -0300

    chore: Add all first letters to uppercase util

--- apps/server/src/utils/string-utils.ts
+++ apps/server/src/utils/string-utils.ts
@@ -1,3 +1,7 @@
 export const firstLetterUppercase = (str: string): string => {
   return `${str[0].toUpperCase()}${str.slice(1, str.length).toLowerCase()}`;
 }
+
+export const allFirstLettersToUppercase = (str: string) => {
+  return str.split(" ").map(st => firstLetterUppercase(st)).join(" ");
+}
