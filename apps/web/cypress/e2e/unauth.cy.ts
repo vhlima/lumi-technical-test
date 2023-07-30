b598d6cb917aea6bda81b053000cb8392329bd20
vhlima <vhlima2000@gmail.com>
2023-07-30 01:42:02 -0300

    test: Ensure links in the navigation bar

--- apps/web/cypress/e2e/unauth.cy.ts
+++ apps/web/cypress/e2e/unauth.cy.ts
@@ -6,4 +6,8 @@ describe('Home page unauthenticated', () => {
     cy.contains('Welcome! Begin your journey by uploading your first invoice.')
       .should('be.visible');
   });
+  it('should have two links in the navigation bar', () => {
+    cy.get('a.MuiButton-root.MuiButton-text.MuiButton-textPrimary')
+      .should('have.length', 2);
+  });
 });
