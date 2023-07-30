e78e318dab5ed4f98ac9ad0c5090be9fa91333aa
vhlima <vhlima2000@gmail.com>
2023-07-29 23:43:24 -0300

    chore: Update mock load pdf

--- apps/server/tests/adapters/mocks/mock-load-pdf.ts
+++ apps/server/tests/adapters/mocks/mock-load-pdf.ts
@@ -1,18 +1,8 @@
-import { PDFDocument } from "@/domain/entities";
 import { LoadPDF } from "@/domain/usecases";
 import { mockInvoice, mockPdfTextContent } from "@/tests/domain/mocks";
 
 export class MockLoadPDF implements LoadPDF {
-  public async execute(path: string): Promise<PDFDocument> {
-    return {
-      getPage: (pageNumber: number) => {
-        return Promise.resolve({
-          pageNumber,
-          getTextContent: async () => {
-            return mockPdfTextContent(mockInvoice());
-          },
-        });
-      },
-    };
+  public async execute(path: string): Promise<string[][]> {
+    return mockPdfTextContent(mockInvoice());
   }
 }
