3b8a17fd03f990008795cb8e1527ba212ba53e1f
vhlima <vhlima2000@gmail.com>
2023-07-29 22:24:24 -0300

    test: Ensure client address is being parsed correctly

---
+++ apps/server/tests/data/services/parse-client-address.spec.ts
@@ -0,0 +1,38 @@
+import { ClientAddressModel } from "@/data/models";
+import { LabelMapperService, ParseClientAddressService } from "@/data/services";
+import { mockInvoice, mockPdfTextContent } from "@/tests/domain/mocks";
+import { allFirstLettersToUppercase } from "@/utils/string-utils";
+import { ClientAddressValidator } from "@/validation/validators";
+
+const createSut = () => {
+  const sut = new ParseClientAddressService(
+    new ClientAddressValidator(),
+    new LabelMapperService()
+  );
+  return sut;
+};
+
+describe("ParseClientAddressService", () => {
+  test("Should parse ClientAddress correctly", async () => {
+    const sut = createSut();
+
+    const mockedInvoice = mockInvoice();
+
+    const pdfTextContent = mockPdfTextContent(mockedInvoice);
+
+    const createAddress = sut.execute(pdfTextContent);
+
+    const addressModel: ClientAddressModel = {
+      id: mockedInvoice.address.id,
+      city: allFirstLettersToUppercase(mockedInvoice.address.city),
+      district: allFirstLettersToUppercase(mockedInvoice.address.district),
+      state: mockedInvoice.address.state,
+      streetAddress: allFirstLettersToUppercase(
+        mockedInvoice.address.streetAddress
+      ),
+      zipCode: mockedInvoice.address.zipCode,
+    };
+
+    expect(createAddress).toEqual(addressModel);
+  });
+});
