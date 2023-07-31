d8107339007c2a0353f325bf26983c76e9be182d
vhlima <vhlima2000@gmail.com>
2023-07-31 04:26:15 -0300

    test: Ensure sign in on invoices page

--- apps/web/cypress/e2e/invoices.cy.ts
+++ apps/web/cypress/e2e/invoices.cy.ts
@@ -3,9 +3,12 @@ import { mockLocalStorage } from "../../tests/mocks";
 describe("Home page unauthenticated", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000/invoices");
-    mockLocalStorage();
+  });
+  it("Should not navigate to invoices", () => {
+    cy.url().should("include", "/");
   });
   it("Should show address section", () => {
+    mockLocalStorage();
     cy.get('[data-testid="street-address"]').should("be.visible");
     cy.get('[data-testid="address-select-button"]').should("be.visible");
   });
